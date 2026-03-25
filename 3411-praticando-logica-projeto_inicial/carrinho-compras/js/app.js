2c360f8d45af7d152aeea84f8843610126ba6630
maribohlhalter <maribohlhalter@gmail.com>
2026-03-25 12:31:27 -0300

    carrinho de compras

---
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -0,0 +1,30 @@
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$0,00';
+
+
+function adicionar() {
+//recuperar valores: nome do produto e quantidade
+let nomeProduto = document.getElementById('produto').value;
+let produto = nomeProduto.split(' - ')[0];
+let valorUnitario = nomeProduto.split('R$')[1];
+let quantidade = document.getElementById('quantidade').value;
+//calcular o preço
+let preco = valorUnitario * quantidade;
+//adicionar no carrinho
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+//atualizar o valor total
+totalGeral = totalGeral + preco;
+let totalCampo = document.getElementById('valor-total');
+totalCampo.innerHTML = `Valor total: R$${totalGeral}`;
+document.getElementById('quantidade').value = '';
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0,00';
+}
